767a00d252f2b0ac27d8021732f38c9f83c8383e
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-01 00:00:46 +0100

    Create 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a new binary tree node.
+ * @parent: pointer to the parent node of the node to create
+ * @value: vale to put in the new node
+ *
+ * Return: a pointer to the new node or NULL on failure .
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	/* create a new node of data type binary_tree_t*/
+	binary_tree_t *newNode;
+
+	newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+	{
+		return NULL;
+	}
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
