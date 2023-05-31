811fe00cb4ab2700399e35f5c9aad394ca9df095
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-01 00:48:39 +0100

    del node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -30,5 +30,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 
 	parent->left = newNode;
+	
 	return newNode;
 }
---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: pointer to the root node of the tree to be deleted
+ *
+ * Description: if tree is Null do nothing
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    /* delete both left and right tree recursively*/
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+
+    free(tree);
+}
