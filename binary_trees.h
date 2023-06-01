f901354c58a00ef6bd12c6956b90642c048b3889
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-01 22:46:30 +0100

    depth

--- binary_trees.h
+++ binary_trees.h
@@ -20,7 +20,8 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-void binary_tree_print(const binary_tree_t *);typedef struct binary_tree_s binary_tree_t;
+void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
