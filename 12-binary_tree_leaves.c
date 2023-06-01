cd7fc06e26156864b3aa697b93cc8654889e8213
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-01 23:08:04 +0100

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - count the numbe rof leaf nodes
+ * @tree: pointer to the root node of the tree
+ *
+ * Return: 0 if tree is NULL
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return 0;
+	}
+
+	/* leaf node both left and right child is NULL*/
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return 1;
+	}
+
+	/* count left and right leaf nodes, then return sum*/
+	size_t left_leaf = binary_tree_leaves(tree->left);
+	size_t right_leaf = binary_tree_leaves(tree->right);
+
+	return (left_leaf + right_leaf);
+}
