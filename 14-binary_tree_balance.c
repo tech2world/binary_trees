f85b3797036844d5f8c5b50ee86e59ef82a11cfc
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 10:40:31 +0100

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -9,15 +9,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height;
+	int right_heigth;
+
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
 	/*Balance factor = height of left sub-tree - height of right sub-tree,
 	using our previously created height function 'binary_tree_height'*/
-
-	int left_height;
-	int right_heigth;
 
 	left_height = binary_tree_height(tree->left);
 	right_heigth = binary_tree_height(tree->right);
