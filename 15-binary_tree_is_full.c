e696f92f4eda432bbc2c81fca3cf604acf28f843
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 16:51:46 +0100

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -25,8 +25,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 	if (tree->left != NULL && tree->right != NULL)
 	{
-		return (binary_tree_is_full(tree->left) && \
-		binary_tree_is_full(tree->right));
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 	}
 	return (0);
 }
