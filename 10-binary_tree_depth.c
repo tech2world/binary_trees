b9386d16313e15d7b62915fd9d8e4a96f30c4ffe
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 08:19:21 +0100

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,17 +9,19 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
+
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
-	size_t depth = 0;
+	depth = 0;
 
 	while (tree->parent != NULL)
 	{
 		depth++;
 		tree = tree->parent;
 	}
-	return depth;
+	return (depth);
 }
