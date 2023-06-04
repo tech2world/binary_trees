c1d912c8a13d5e5e27e6596914968848771008bf
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 07:58:28 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,25 +9,39 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height = 0;
+	size_t right_height = 0;
 
 	if (tree == NULL)
 	{
-		return (0);
+		return 0;
 	}
 
 	/* calculates height of left and right sub-trees recursively*/
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	/* return the highest + 1 (root)*/
-	if (left_height > right_height)
+	if (tree->left != NULL)
 	{
-		return (1 + left_height);
+		left_height = 1 + binary_tree_height(tree->left);
 	}
 	else
 	{
-		return (1 + right_height);
+		left_height = 0;
+	}
+
+	if (tree->right != NULL)
+	{
+		right_height = 1 + binary_tree_height(tree->right);
+	}
+	else
+	{
+		right_height = 0;
+	}
+	/* return the highest height*/
+	if (left_height > right_height)
+	{
+		return (left_height);
+	}
+	else
+	{
+		return (right_height);
 	}
 }
