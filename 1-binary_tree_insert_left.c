73f132032eaac2b699dc642e5ca6526e64af3a01
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 07:12:07 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,12 +10,13 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *newNode;
+	
 	if (parent == NULL)
 	{
 		return NULL;
 	}
 
-	binary_tree_t *newNode;
 	newNode = binary_tree_node(parent, value);
 
 	if (newNode == NULL)
