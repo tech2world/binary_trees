9d3b556fe317f0757e380bcd1fffc6b1f326e92c
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 10:20:18 +0100

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -20,8 +20,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	}
 
 /* check if the current node has at least one child,
-/*if it does count it has as a node and recursively count the nodes in the*/
-/*left and right sub-trees*/
+if it does count it has as a node and recursively count the nodes in
+the left and right sub-trees
+*/
 
 	if (tree->left != NULL || tree->right != NULL)
 	{
