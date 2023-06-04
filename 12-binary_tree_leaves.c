422af4fdaac3efb890ea3e9fdb8ddd8705cc93be
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 10:06:10 +0100

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,20 +9,23 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t left_leaf;
+	size_t right_leaf;
+
 	if (tree == NULL)
 	{
-		return 0;
+		return (0);
 	}
 
 	/* leaf node both left and right child is NULL*/
 	if (tree->left == NULL && tree->right == NULL)
 	{
-		return 1;
+		return (1);
 	}
 
 	/* count left and right leaf nodes, then return sum*/
-	size_t left_leaf = binary_tree_leaves(tree->left);
-	size_t right_leaf = binary_tree_leaves(tree->right);
+	left_leaf = binary_tree_leaves(tree->left);
+	right_leaf = binary_tree_leaves(tree->right);
 
 	return (left_leaf + right_leaf);
 }
