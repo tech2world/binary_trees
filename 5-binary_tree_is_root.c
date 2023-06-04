797881d44ca6532259ced6d54ae7debe0babb6ba
Abdulazeez ibrahim <abdulazeezibrahim847@gmail.com>
2023-06-04 10:01:02 +0100

    Update 5-binary_tree_is_root.c

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -5,21 +5,21 @@
  * @node: pointer to the node to check
  *
  * Return: 1 if node is a root otherwise 0
-*/
+ */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    /* if null, root does not exists*/
-    if (node == NULL)
-    {
-        return (0);
-    }
+	/* if null, root does not exists*/
+	if (node == NULL)
+	{
+		return (0);
+	}
 
-    /* check for parent, if none then it is a root*/
-    if (node->parent == NULL)
-    {
-        return (1);
-    }
-    /* parent exists - not root*/
-    return (0);
+	/* check for parent, if none then it is a root*/
+	if (node->parent == NULL)
+	{
+		return (1);
+	}
+	/* parent exists - not root*/
+	return (0);
 }
